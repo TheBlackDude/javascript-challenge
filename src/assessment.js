9bed72b9cbefd24f00226a99e8694dc584982541
Ousmane Diallo <mudu.jalloh@gn.ehealthafrica.org>
2017-11-03 00:54:24 +0800

    finish: commit.

--- src/assessment.js
+++ src/assessment.js
@@ -1,5 +1,96 @@
-class Assessment {
+import dimensions from './dimensions'
 
+class Assessment {
+  constructor() {
+    this.AssessmentResult = {
+      'Adaptive': 0, 'Integrity': 0,
+      'Collaborative': 0, 'Result': 0,
+      'Customer': 0, 'Detail': 0
+    };
+    this.questions = []
+    this.dimension = null
+    this.question = null
+  }
+
+  generateQuestions() {
+    const dimensionTags = [
+      'Adaptive', 'Integrity', 'Collaborative',
+      'Result', 'Customer', 'Detail'
+    ]
+    let i = 0
+    while (i < 30) {
+      this.questions.push({
+        title: 'Pick the answer that describes you best:',
+        option1: {
+          dimension: this.getRandomItem(dimensionTags),
+          answer: this.getRandomAnswer(this.dimension)
+        },
+        option2: {
+          dimension: this.getRandomItem(dimensionTags),
+          answer: this.getRandomAnswer(this.dimension)
+        }
+      })
+      i++
+    }
+  }
+
+  getRandomAnswer(dimension) {
+    let answer = null
+    dimensions.filter(value => {
+      if (value.name === dimension) {
+        answer = this.getRandomItem(value.answers)
+      }
+    })
+    return answer
+  }
+
+  getRandomItem(items) {
+    let value = items[Math.round(Math.random()*items.length)]
+    if (value === this.dimension) {
+      value = items[Math.floor(Math.random()*items.length)]
+    }
+    this.dimension = value
+    return value
+  }
+
+  getQuestion(questions) {
+    let question = questions[Math.round(Math.random()*questions.length)]
+    if (question === this.question) {
+      question = questions[Math.floor(Math.random()*questions.length)]
+    }
+    this.question = question
+    return question
+  }
+
+  chooseAnswer(answer) {
+    switch (answer.dimension) {
+      case 'Adaptive':
+        this.AssessmentResult['Adaptive'] += 1
+        break;
+      case 'Integrity':
+        this.AssessmentResult['Integrity'] += 1
+        break;
+      case 'Collaborative':
+        this.AssessmentResult['Collaborative'] += 1
+        break;
+      case 'Result':
+        this.AssessmentResult['Result'] += 1
+        break;
+      case 'Customer':
+        this.AssessmentResult['Customer'] += 1
+        break;
+      case 'Detail':
+        this.AssessmentResult['Detail'] += 1
+        break;
+      default:
+        this.AssessmentResult = {
+          'Adaptive': 0, 'Integrity': 0,
+          'Collaborative': 0, 'Result': 0,
+          'Customer': 0, 'Detail': 0
+        }
+    }
+    return this.AssessmentResult
+  }
 }
 
 export default Assessment;
--- test/assessment.test.js
+++ test/assessment.test.js
@@ -2,24 +2,62 @@ import Assessment from '../src/assessment'
 
 // Feel free to rewrite this test suite. This is provided as guidance.
 describe('The Assessment', () => {
-  it('should have 30 questions', () => {
-    expect('this test').toBe('failing');
-  });
-  it('should not show the same answer twice', () => {
-    expect('this test').toBe('failing');   
-  });
-  it('should match each dimension to the other dimensions exactly 2 times', () => {
-    expect('this test').toBe('failing');   
-  });
-  it('should provide ipsative questions (two possible answers)', () => {
-    expect('this test').toBe('failing');   
-  });
-  describe('when completed', () => {
-    it('should provide the results as an object', () => {
-      expect('this test').toBe('failing');   
+  describe('generateQuestions', () => {
+    it('should generate 30 questions', () => {
+      const assessment = new Assessment()
+      assessment.generateQuestions()
+      expect(assessment.questions).toHaveLength(30);
+      expect(assessment.questions).not.toHaveLength(31)
     });
-    it('should represent the results based on 6 dimensions', () => {
-      expect('this test').toBe('failing');   
+  })
+  describe('getQuestion', () => {
+    it('should get random questions', () => {
+      const assessment = new Assessment()
+      assessment.generateQuestions()
+      const question1 = assessment.getQuestion(assessment.questions)
+      const question2 = assessment.getQuestion(assessment.questions)
+      expect(question1).not.toEqual(question2)
+    });
+    it('should provide two possible answers', () => {
+      const assessment = new Assessment()
+      assessment.generateQuestions()
+      const question = assessment.getQuestion(assessment.questions)
+      expect(question.option1).toBeDefined()
+      expect(question.option2).toBeDefined()
+    });
+    it('should always display different answers', () => {
+      const assessment = new Assessment()
+      assessment.generateQuestions()
+      const question1 = assessment.getQuestion(assessment.questions)
+      const question2 = assessment.getQuestion(assessment.questions)
+      expect(question1.option1.answer).not.toEqual(question2.option1.answer)
+    });
+  });
+  describe('chooseAnswer', () => {
+    it('should increment dimension based on answer', () => {
+      const assessment = new Assessment()
+      assessment.generateQuestions()
+      const question = assessment.getQuestion(assessment.questions)
+      const defaultResults = {
+        'Adaptive': 0, 'Integrity': 0,
+        'Collaborative': 0, 'Result': 0,
+        'Customer': 0, 'Detail': 0
+      }
+      expect(assessment.AssessmentResult).toEqual(defaultResults)
+      assessment.chooseAnswer(question.option1)
+      assessment.chooseAnswer(question.option1)
+      assessment.chooseAnswer(question.option2)
+      expect(assessment.AssessmentResult).not.toEqual(defaultResults)
+    });
+    it('should return defaultResults if answer is not found', () => {
+      const assessment = new Assessment()
+      const defaultResults = {
+        'Adaptive': 0, 'Integrity': 0,
+        'Collaborative': 0, 'Result': 0,
+        'Customer': 0, 'Detail': 0
+      }
+      assessment.chooseAnswer('not a possible answer')
+      expect(assessment.AssessmentResult).toEqual(defaultResults)
     });
   });
 });
